6c7e489ccfaffa0fe1a991689ddb4d31cb864635
Dmitry Zinkevich <mr.zinch@protonmail.com>
2021-08-16 22:52:24 +0300

    test if a tuple is a vector or a point

--- core/tuple.go
+++ core/tuple.go
@@ -4,5 +4,13 @@ type Tuple struct {
 	X float32
 	Y float32
 	Z float32
-    W int
+	W int
+}
+
+func (t Tuple) IsVector() bool {
+	return t.W == 0
+}
+
+func (t Tuple) IsPoint() bool {
+	return t.W == 1
 }
--- test/tuple_test.go
+++ test/tuple_test.go
@@ -1,14 +1,27 @@
 package core
 
 import (
-    "testing"
-    "example.com/ray-tracer/core"
+	"testing"
+
+	"example.com/ray-tracer/core"
 )
 
-func TestTupleCreation(t *testing.T) {
-    var t1 = core.Tuple{1, 2, 3, 1}
-    var t2 = core.Tuple{1, 2, 3, 1}
-    if t1.W != t2.W {
-        t.Errorf("Tuples not equal")
+func Test_tuple_with_w_equal_1_is_a_point(t *testing.T) {
+	var tuple = core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1}
+	if !tuple.IsPoint() {
+		t.Fatalf("%v must be a point", tuple)
+	}
+    if tuple.IsVector() {
+		t.Fatalf("%v cannot be a vector", tuple)
+    }
+}
+
+func Test_tuple_with_w_equal_0_is_a_vector(t *testing.T) {
+	var tuple = core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 0}
+	if tuple.IsPoint() {
+		t.Fatalf("%v cannot be a point", tuple)
+	}
+    if !tuple.IsVector() {
+		t.Fatalf("%v must be a vector", tuple)
     }
 }
