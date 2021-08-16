b06b706d7b847013f6233ae15abe4f94bda0a1f1
Dmitry Zinkevich <mr.zinch@protonmail.com>
2021-08-16 22:57:01 +0300

    use local variable declaration

--- test/tuple_test.go
+++ test/tuple_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test_tuple_with_w_equal_1_is_a_point(t *testing.T) {
-	var tuple = core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1}
+	tuple := core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1}
 	if !tuple.IsPoint() {
 		t.Fatalf("%v must be a point", tuple)
 	}
@@ -17,7 +17,7 @@ func Test_tuple_with_w_equal_1_is_a_point(t *testing.T) {
 }
 
 func Test_tuple_with_w_equal_0_is_a_vector(t *testing.T) {
-	var tuple = core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 0}
+	tuple := core.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 0}
 	if tuple.IsPoint() {
 		t.Fatalf("%v cannot be a point", tuple)
 	}
