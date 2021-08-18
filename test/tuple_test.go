48d06a96e767976f3f53d21341acfd0625a7e9ca
Dmitry Zinkevich <mr.zinch@protonmail.com>
2021-08-18 23:37:16 +0300

    introduce fluent assertion API to improve test readability

--- test/tuple_test.go
+++ test/tuple_test.go
@@ -27,19 +27,15 @@ func Test_tuple_with_w_equal_0_is_a_vector(t *testing.T) {
 }
 
 func Test_point_creates_a_tuple_with_w_equal_1(t *testing.T) {
-	tuple := core.Point(4, -4, 3)
+	result := core.Point(4, -4, 3)
 	expected := core.Tuple{X: 4, Y: -4, Z: 3, W: 1}
-	if !tuple.Equals(expected) {
-		t.Fatalf("%v must be equal to %v", tuple, expected)
-	}
+	with(t).assertThat(result).isEqualTo(expected)
 }
 
 func Test_vector_creates_a_tuple_with_w_equal_0(t *testing.T) {
-	tuple := core.Vector(4, -4, 3)
+	result := core.Vector(4, -4, 3)
 	expected := core.Tuple{X: 4, Y: -4, Z: 3, W: 0}
-	if !tuple.Equals(expected) {
-		t.Fatalf("%v must be equal to %v", tuple, expected)
-	}
+	with(t).assertThat(result).isEqualTo(expected)
 }
 
 func Test_adding_two_tuples(t *testing.T) {
@@ -47,9 +43,7 @@ func Test_adding_two_tuples(t *testing.T) {
 	t2 := core.Vector(-2, 3, 1)
 	result := t1.Plus(t2)
 	expected := core.Point(1, 1, 6)
-	if !result.Equals(expected) {
-		t.Fatalf("%v must be equal to %v", result, expected)
-	}
+	with(t).assertThat(result).isEqualTo(expected)
 }
 
 func Test_subtracting_two_points(t *testing.T) {
@@ -57,7 +51,29 @@ func Test_subtracting_two_points(t *testing.T) {
 	t2 := core.Point(5, 6, 7)
 	result := t1.Minus(t2)
 	expected := core.Vector(-2, -4, -6)
-	if !result.Equals(expected) {
-		t.Fatalf("%v must be equal to %v", result, expected)
+	with(t).assertThat(result).isEqualTo(expected)
+}
+
+func with(t *testing.T) Assert {
+	return Assert{Test: t}
+}
+
+type Assert struct {
+	Test *testing.T
+}
+
+func (assert Assert) assertThat(t core.Tuple) TupleMatcher {
+	return TupleMatcher{Assert: assert, Tuple: t}
+}
+
+type TupleMatcher struct {
+	Assert
+	Tuple core.Tuple
+}
+
+func (matcher TupleMatcher) isEqualTo(expected core.Tuple) {
+	t := matcher.Assert.Test
+	if !matcher.Tuple.Equals(expected) {
+		t.Fatalf("%v must be equal to %v", matcher.Tuple, expected)
 	}
 }
