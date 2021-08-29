7c17e4b62a58268c6b0dc390d0dfeb3003a5e3eb
Dmitry Zinkevich <mr.zinch@protonmail.com>
2021-08-29 11:43:45 +0300

    write color to a canvas

--- graphics/canvas.go
+++ graphics/canvas.go
@@ -3,8 +3,22 @@ package graphics
 type Canvas struct {
 	Width  int
 	Height int
+	colors []Color
 }
 
 func NewCanvas(width int, height int) Canvas {
-	return Canvas{width, height}
+	colors := make([]Color, width*height)
+	return Canvas{width, height, colors}
+}
+
+func (canvas *Canvas) WritePixel(x int, y int, color Color) {
+	canvas.colors[canvas.pixelIndex(x, y)] = color
+}
+
+func (canvas *Canvas) PixelAt(x int, y int) Color {
+	return canvas.colors[canvas.pixelIndex(x, y)]
+}
+
+func (canvas *Canvas) pixelIndex(x int, y int) int {
+	return x*canvas.Width + y
 }
--- test/canvas_test.go
+++ test/canvas_test.go
@@ -11,3 +11,10 @@ func Test_creating_a_canvas(t *testing.T) {
 	test(t).that(c.Width).isEqualTo(10)
 	test(t).that(c.Height).isEqualTo(20)
 }
+
+func Test_writing_pixels_to_canvas(t *testing.T) {
+	c := graphics.NewCanvas(10, 20)
+	red := graphics.NewColor(1, 0, 0)
+	c.WritePixel(2, 3, red)
+	test(t).that(c.PixelAt(2, 3)).isEqualTo(red)
+}
